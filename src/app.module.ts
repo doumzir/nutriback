d113499d82ab0f01a74f8721ccbbc6c2be7cbf35
David D'HOOGHE <ddhooghe@access-it.fr>
2024-06-13 11:24:21 +0200

    delete usertest folder

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UserModule } from './user/user.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { Module } from "@nestjs/common";
+import { UserModule } from "./user/user.module";
+import { PrismaModule } from "./prisma/prisma.module";
 
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-  imports: [UserModule, PrismaModule, AuthModule],
-  controllers: [],
-  providers: [],
+	imports: [UserModule, PrismaModule, AuthModule],
+
+	controllers: [],
+	providers: [],
 })
 export class AppModule {}
